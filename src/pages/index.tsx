32bf907a4cdbbb0e4cb582f79d1c2aef38bd6258
Suzuki Kazuto <suzuki.kazuto@fujits.com>
2023-05-15 18:13:08 +0900

    スマートフォンの場合は画像を1枚ずつ出す様に変更。レビュアーかつ緊急修正のためmainブランチで修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function index() {
           変ないきもの水族館
         </div>
       </header>
-      <div className="flex flex-wrap justify-start">
+      <div className="flex-wrap justify-center md:flex flex-wrap justify-start lg:flex-wrap justify-start">
         {animals.map((animal, index) => {
           return (
-            <div key={index} className="w-1/2 md:w-1/3 lg:w-1/4 p-2">
+            <div key={index} className="w-1/1 md:w-1/3 lg:w-1/4 p-2">
               <div className="px-4 py-2 bg-white rounded-lg shadow-md overflow-hidden">
                 <Card
                   picture_adress={animal.picture_adress}
